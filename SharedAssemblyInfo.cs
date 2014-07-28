893be5932a2efe2e0c10bf1efe71adaf7644a49a
Matt Johnson <mj1856@hotmail.com>
2014-07-27 21:25:52 -0700

    Fix version wildcarding

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Time Zone Names")]
 [assembly: AssemblyCopyright("Copyright © 2014 Matt Johnson")]
 
-[assembly: AssemblyFileVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
