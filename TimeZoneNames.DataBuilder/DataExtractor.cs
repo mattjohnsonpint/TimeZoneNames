7a8f94edfeb1ec1dce17d0711c8270c40f8cc8b4
Matt Johnson <mj1856@hotmail.com>
2016-04-05 15:58:20 -0700

    Patch and selection data extraction

--- TimeZoneNames.DataBuilder/DataExtractor.cs
+++ TimeZoneNames.DataBuilder/DataExtractor.cs
@@ -72,6 +72,12 @@ namespace TimeZoneNames.DataBuilder
                 LoadLanguages
             };
             await Task.WhenAll(actions.Select(Task.Run));
+
+            // patch the data for any known issues
+            PatchData();
+
+            // this has to come last, as it relies on patched data
+            LoadSelectionZones();
         }
 
         private async Task DownloadDataAsync()
@@ -93,11 +99,98 @@ namespace TimeZoneNames.DataBuilder
             {
                 AddToLookup(_data.TzdbZoneCountries, location.ZoneId, location.CountryCode);
             }
+        }
+
+        private static readonly Instant Now = SystemClock.Instance.Now;
+        private static readonly Instant Jan = Instant.FromUtc(Now.InUtc().Year, 1, 1, 0, 0);
+        private static readonly Instant Jun = Instant.FromUtc(Now.InUtc().Year, 6, 1, 0, 0);
+
+        private Offset GetStandardOffset(string zoneId)
+        {
+            return _tzdbProvider[zoneId].GetZoneInterval(Now).StandardOffset;
+        }
+
+        private Offset GetDaylightOffset(string zoneId)
+        {
+            var zone = _tzdbProvider[zoneId];
+            return Offset.Max(zone.GetZoneInterval(Jan).WallOffset, zone.GetZoneInterval(Jun).WallOffset);
+        }
+
+        private void LoadSelectionZones()
+        {
+            var y = Now.InUtc().Year;
+            var a = y;
+            var b = y + 5;
+
+            var results = _tzdbProvider.Ids
+                .Select(x => _tzdbSource.CanonicalIdMap[x])
+                .Distinct()
+                .Select(x => new
                 {
+                    Id = x,
+                    Intervals = GetBoundIntervals(_tzdbProvider[x], a, b),
+                    Location = _tzdbSource.ZoneLocations.FirstOrDefault(l => l.ZoneId == x)
+                })
+                .Where(x => x.Location != null)
+                .GroupBy(x => new { x.Location.CountryCode, Hash = GetIntervalsHash(x.Intervals) })
+                .Select(g =>
+                {
+                    var ids = g.Select(z => z.Id).ToArray();
+                    if (ids.Length == 1)
+                        return ids[0];
 
+                    return ids.FirstOrDefault(id => HasWindowsMapping(id, g.Key.CountryCode)) ?? ids[0];
+                })
+                .ToList();
 
+            // Unfortunately, some zones are too politically charged to appear in most selection lists.
+            results.Remove("Asia/Urumqi");
+            results.Remove("Europe/Simferopol");
+
+            _data.SelectionZones.AddRange(results);
+        }
+
+        private bool HasWindowsMapping(string id, string countryCode)
+        {
+            var territory = _data.CldrWindowsMappings.ContainsKey(countryCode) ? countryCode : "001";
+            return _data.CldrWindowsMappings[territory].Values.Contains(id);
+        }
+
+        private static int GetIntervalsHash(IEnumerable<ZoneInterval> intervals)
+        {
+            int hash = 17;
+            unchecked
+            {
+                foreach (var interval in intervals)
+                {
+                    hash = hash * 23 + interval.Start.GetHashCode();
+                    hash = hash * 23 + interval.End.GetHashCode();
+                    hash = hash * 23 + interval.WallOffset.GetHashCode();
+                    hash = hash * 23 + interval.Savings.GetHashCode();
                 }
             }
+            return hash;
+        }
+
+        private static IEnumerable<ZoneInterval> GetBoundIntervals(DateTimeZone zone, int startLocalYear, int endLocalYear)
+        {
+            var start = new LocalDate(startLocalYear, 1, 1).AtMidnight().InZoneLeniently(zone).ToInstant();
+            var end = new LocalDate(endLocalYear, 1, 1).AtMidnight().InZoneLeniently(zone).ToInstant();
+            var intervals = zone.GetZoneIntervals(start, end).ToList();
+
+            var first = intervals.First();
+            if (first.Start == Instant.MinValue || first.Start < start)
+            {
+                intervals[0] = new ZoneInterval(first.Name, start, first.End, first.WallOffset, first.Savings);
+            }
+
+            var last = intervals.Last();
+            if (last.End == Instant.MaxValue || last.End > end)
+            {
+                intervals[intervals.Count - 1] = new ZoneInterval(last.Name, last.Start, end, last.WallOffset, last.Savings);
+            }
+
+            return intervals;
         }
 
         private void LoadZoneAliases()
@@ -113,7 +206,10 @@ namespace TimeZoneNames.DataBuilder
                         continue;
 
                     var aliases = aliasAttribute.Value.Split(' ');
-                    foreach (var alias in aliases)
+                    if (aliases.Length == 1)
+                        continue;
+
+                    foreach (var alias in aliases.Skip(1))
                         _data.CldrAliases.Add(alias.ToLowerInvariant(), aliases[0]);
                 }
             }
@@ -124,7 +220,7 @@ namespace TimeZoneNames.DataBuilder
             using (var stream = File.OpenRead(_cldrPath + @"common\supplemental\metaZones.xml"))
             {
                 var doc = XDocument.Load(stream);
-                
+
                 var timeZoneElements = doc.XPathSelectElements("/supplementalData/metaZones/metazoneInfo/timezone");
                 foreach (var element in timeZoneElements)
                 {
@@ -138,8 +234,16 @@ namespace TimeZoneNames.DataBuilder
                 {
                     var timeZone = element.Attribute("type").Value;
                     var territory = element.Attribute("territory").Value;
-                    if (territory.Length == 2 && !_data.CldrZoneCountries.ContainsKey(timeZone))
-                        _data.CldrZoneCountries.Add(timeZone, territory);
+                    if (territory.Length == 2)
+                        AddToLookup(_data.CldrZoneCountries, timeZone, territory);
+                }
+
+                var primaryZoneElements = doc.XPathSelectElements("/supplementalData/metaZones/primaryZones/primaryZone");
+                foreach (var element in primaryZoneElements)
+                {
+                    var country = element.Attribute("iso3166").Value;
+                    var zone = element.Value;
+                    _data.CldrPrimaryZones.Add(country, zone);
                 }
             }
         }
@@ -156,8 +260,15 @@ namespace TimeZoneNames.DataBuilder
                     var windowsZone = element.Attribute("other").Value;
                     var timeZone = element.Attribute("type").Value.Split().First();
                     var territory = element.Attribute("territory").Value;
-                    if (territory == "001") // we only care about the primary territory mapping in this library
-                        _data.CldrWindowsMappings.Add(windowsZone, timeZone);
+
+                    Dictionary<string, string> mappings;
+                    if (!_data.CldrWindowsMappings.TryGetValue(territory, out mappings))
+                    {
+                        mappings = new Dictionary<string, string>();
+                        _data.CldrWindowsMappings.Add(territory, mappings);
+                    }
+
+                    mappings.Add(windowsZone, timeZone);
                 }
             }
         }
@@ -182,11 +293,12 @@ namespace TimeZoneNames.DataBuilder
                 {
                     AddCountryEntries(territoriesElement, language);
                 }
-                
+
                 var tzElement = doc.XPathSelectElement("/ldml/dates/timeZoneNames");
                 if (tzElement != null)
                 {
                     AddFormatEntries(tzElement, language);
+                    AddZoneEntries(tzElement, language, "zone", "exemplarCity");
                     AddZoneEntries(tzElement, language, "zone", "short");
                     AddZoneEntries(tzElement, language, "zone", "long");
                     AddZoneEntries(tzElement, language, "metazone", "short");
@@ -197,19 +309,17 @@ namespace TimeZoneNames.DataBuilder
 
         private void AddCountryEntries(XContainer territoriesElement, string language)
         {
+            var excluded = new[] { "AN", "BV", "CP", "EU", "HM", "QO", "ZZ" };
+
             var countries = territoriesElement.Elements("territory")
                 .GroupBy(x => x.Attribute("type").Value)
-                .Where(x => x.Key.Length == 2)
+                .Where(x => x.Key.Length == 2 && !excluded.Contains(x.Key))
                 .ToDictionary(x => x.Key, x => x.Last().Value);
 
             var langData = GetLangData(language);
             foreach (var country in countries)
             {
-                // we only need some of the territory names
-                if (_data.CldrZoneCountries.ContainsValue(country.Key))
-                {
-                    langData.CountryNames.Add(country.Key, country.Value);
-                }
+                langData.CountryNames.Add(country.Key, country.Value);
             }
         }
 
@@ -222,7 +332,7 @@ namespace TimeZoneNames.DataBuilder
 
             if (formats.Count == 0)
                 return;
-            
+
             string s;
             var values = new TimeZoneValues();
             if (formats.TryGetValue("generic", out s))
@@ -234,10 +344,16 @@ namespace TimeZoneNames.DataBuilder
 
             var langData = GetLangData(language);
             langData.Formats = values;
+
+            var fallbackFormat = tzElement.Element("fallbackFormat");
+            if (fallbackFormat != null)
+                langData.FallbackFormat = fallbackFormat.Value;
         }
 
         private void AddZoneEntries(XContainer tzElement, string language, string elementName, string entryName)
         {
+            var langData = GetLangData(language);
+
             var zones = tzElement.Elements(elementName);
             foreach (var zone in zones)
             {
@@ -247,18 +363,25 @@ namespace TimeZoneNames.DataBuilder
                 if (element == null)
                     continue;
 
-                var values = GetTimeZoneValues(element);
-
-                var langData = GetLangData(language);
-
                 switch (entryName)
                 {
+                    case "exemplarCity":
+                        {
+                            langData.CityNames.Add(zoneName, element.Value);
+                            break;
+                        }
                     case "short":
-                        langData.ShortNames.Add(zoneName, values);
-                        break;
+                        {
+                            var values = GetTimeZoneValues(element);
+                            langData.ShortNames.Add(zoneName, values);
+                            break;
+                        }
                     case "long":
-                        langData.LongNames.Add(zoneName, values);
-                        break;
+                        {
+                            var values = GetTimeZoneValues(element);
+                            langData.LongNames.Add(zoneName, values);
+                            break;
+                        }
                 }
             }
         }
@@ -280,26 +403,161 @@ namespace TimeZoneNames.DataBuilder
                 return data;
             }
 
-            
+
         }
 
         private static TimeZoneValues GetTimeZoneValues(XContainer element)
         {
             var values = new TimeZoneValues();
-            
+
             var genericElement = element.Element("generic");
             if (genericElement != null && genericElement.Value != "∅∅∅")
                 values.Generic = genericElement.Value;
-            
+
             var standardElement = element.Element("standard");
             if (standardElement != null && standardElement.Value != "∅∅∅")
                 values.Standard = standardElement.Value;
-            
+
             var daylightElement = element.Element("daylight");
             if (daylightElement != null && daylightElement.Value != "∅∅∅")
                 values.Daylight = daylightElement.Value;
-            
+
             return values;
+        }
+
+        private void PatchData()
+        {
+            //// Fixup Abbreviations
+            //foreach(var key in _data.CldrLanguageData.Keys.Where(x=> x.Length == 2))
+            //{
+            //    var lang = key;
+            //    var subkeys = _data.CldrLanguageData.Keys.Where(x =>
+            //        x.StartsWith(lang + "_", StringComparison.OrdinalIgnoreCase)).ToArray();
+
+            //    if (subkeys.Length == 0) continue;
+
+            //    var zonesWithShortNames = subkeys.SelectMany(x => _data.CldrLanguageData[x].ShortNames)
+            //        .Select(x => x.Key).Distinct().ToArray();
+
+            //    if (zonesWithShortNames.Length == 0) continue;
+
+            //    var zonesWithoutShortNamesInRoot = zonesWithShortNames.Where(x =>
+            //        !_data.CldrLanguageData[lang].ShortNames.ContainsKey(x));
+
+            //    //TODO
+            //    // get countries for these zones
+            //    // get short names for the language within the country
+            //    // remap to the root language
+            //}
+
+            // Add additional mappings to support obsolete Windows time zone IDs
+            _data.CldrWindowsMappings["001"].Add("Mid-Atlantic Standard Time", "Etc/GMT+2");
+            _data.CldrWindowsMappings["001"].Add("Kamchatka Standard Time", "Asia/Kamchatka");
+
+            // Support still-valid old-school ids that cldr doesn't recognize
+            // See https://github.com/eggert/tz/blob/2015g/europe#L628-L634
+            // Note, these are not quite perfect mappings, but good enough for naming purposes
+            _data.CldrAliases["cet"] = "Europe/Paris";
+            _data.CldrAliases["eet"] = "Europe/Bucharest";
+            _data.CldrAliases["met"] = "Europe/Berlin";
+            _data.CldrAliases["wet"] = "Atlantic/Canary";
+
+            // Additional country mappings not in zone.tab
+            AddToLookup(_data.TzdbZoneCountries, "Africa/Ceuta", "EA");
+            AddToLookup(_data.TzdbZoneCountries, "Atlantic/Canary", "IC");
+            AddToLookup(_data.TzdbZoneCountries, "Atlantic/St_Helena", "AC");
+            AddToLookup(_data.TzdbZoneCountries, "Atlantic/St_Helena", "TA");
+            AddToLookup(_data.TzdbZoneCountries, "Europe/Belgrade", "XK");
+            AddToLookup(_data.TzdbZoneCountries, "Indian/Chagos", "DG");
+
+            // Support UTC - Not in CLDR!
+            using (var file = File.OpenRead(@"data\utc.txt"))
+            using (var reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                        break;
+
+                    var parts = line.Split(',');
+                    if (parts.Length == 2)
+                        AddStandardGenericName(parts[0], "Etc/GMT", parts[1]);
+                }
+            }
+
+            // Support localizations of cities for new zones not yet in CLDR
+            using (var file = File.OpenRead(@"data\cities.txt"))
+            using (var reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                        break;
+
+                    var parts = line.Split(',');
+                    if (parts.Length == 3)
+                    {
+                        var zone = _tzdbProvider.Ids.FirstOrDefault(x => x.EndsWith("/" + parts[0]));
+                        AddCityName(parts[1], zone, parts[2]);
+                    }
+                }
+            }
+        }
+
+        private void AddStandardGenericName(string language, string zone, string name)
+        {
+            try
+            {
+                var langKey = NormalizeLangKey(language);
+                var values = new TimeZoneValues { Generic = name, Standard = name };
+                _data.CldrLanguageData[langKey].LongNames.Add(zone, values);
+            }
+            catch
+            {
+                Console.WriteLine("No CLDR data for language " + language);
+            }
+        }
+
+        private void AddCityName(string language, string zone, string name)
+        {
+            try
+            {
+                var langKey = NormalizeLangKey(language);
+                var cityNames = _data.CldrLanguageData[langKey].CityNames;
+                if (!cityNames.ContainsKey(zone))
+                    cityNames.Add(zone, name);
+            }
+            catch
+            {
+                Console.WriteLine("No CLDR data for language " + language);
+            }
+        }
+
+        private string NormalizeLangKey(string language)
+        {
+            var langKey = language.Replace("-", "_").ToLowerInvariant();
+            if (!_data.CldrLanguageData.ContainsKey(langKey))
+                langKey = langKey.Split('_')[0];
+
+            return langKey;
+        }
+
+        private static void AddToLookup<TKey, TValue>(IDictionary<TKey, TValue[]> lookup, TKey key, TValue value)
+        {
+            TValue[] items;
+            if (lookup.TryGetValue(key, out items))
+            {
+                var temp = new TValue[items.Length + 1];
+                items.CopyTo(temp, 0);
+                temp[items.Length] = value;
+                lookup[key] = temp;
+            }
+            else
+            {
+                lookup.Add(key, new[] { value });
+            }
         }
     }
 }
