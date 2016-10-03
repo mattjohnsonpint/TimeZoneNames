6bb4cb6c1f6afe94650e50db91192231743246bc
David Greenwell <dlgreenwell@xactware.com>
2016-10-03 15:05:20 +0000

    #18 Increment SharedAssemblyInfo

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Time Zone Names")]
 [assembly: AssemblyCopyright("Copyright © Matt Johnson")]
 
-[assembly: AssemblyVersion("2.1.0.*")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.1.*")]
+[assembly: AssemblyInformationalVersion("2.1.1")]
